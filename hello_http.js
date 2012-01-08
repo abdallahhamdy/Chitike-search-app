bdf65de823f3fe69fb2a59c38023e1c6f4549503
a <abdallah.hamdy@hotmail.com>
2012-01-08 19:34:43 +0000

    Sync search with request.timeout for return time

--- hello_http.js
+++ hello_http.js
@@ -1,10 +1,10 @@
 var sys = require("util"),
     http = require("http"),
-    async = require('./async/lib/async'), // par request *failed to add library into repos on github*
+    async = require('./async/lib/async'), // parrallel request
     events = require("events");
 
 var rl = require('readline');
-var timeoutReached = false;  // Service level agreemet initilization 
+//var timeoutReached = false;  // Service level agreemet initilization 
 
 var client = http.createClient(80, "github.com"),
     body = "";
@@ -16,16 +16,17 @@ var clientF = http.createClient(80, "api.flickr.com"),
 var i = rl.createInterface(process.stdin, process.stdout, null);
 i.question("Input search term to use for Twitter, Flickr and GitHub: ", function(answer) {
 async.parallel([
-function getResults(callback) {
+function getResults() {
 //clearInterval(interval);
   var request = client.request("GET", "/api/v2/json/repos/search/"+answer);
+  request.setTimeout(100); // return 100 ms
   request.on('response', function (response) {
   var body = '';
   response.on('data', function (chunk) {
     body += chunk;
   });
   response.on('end', function () {
-  console.log(JSON.parse(body));
+	console.log(JSON.parse(body));
 console.log ('****************************************************');
 console.log('                 GIITHUB                             ');
 console.log ('****************************************************');
@@ -33,12 +34,12 @@ console.log ('****************************************************');
 
 });
 request.end(); // start the request;
-
 },
 
-function getResultsT(callback) {
+function getResultsT() {
 //clearInterval(intervalT);
   var requestT = clientT.request("GET", "/search.json?q="+answer); 
+  requestT.setTimeout(100);
  requestT.on('response', function (response) {
   var bodyT = '';
   response.on('data', function (chunk) {
@@ -57,10 +58,11 @@ requestT.end();
 } ,
 
 
-function getResultsF(callback) {
+function getResultsF() {
 //clearInterval(intervalF);
   var requestF = clientF.request("GET", "/services/rest/?method=flickr.photos.search&api_key=b3173fd953498c251b4303252342eec7&per_page=3&format=json&tags="+answer); 
- requestF.on('response', function (response) {
+requestF.setTimeout(100);
+requestF.on('response', function (response) {
   var bodyF = '';
   response.on('data', function (chunk) {
     bodyF += chunk;
@@ -74,15 +76,14 @@ console.log ('****************************************************');
 
 });
 requestF.end(); 
-
-},
+}/*,
 function(callback) {
             setTimeout(function() {
               if(!timeoutReached) {
                 sys.puts("Timeout reached");
               }
             }, 100);
-          }
+          } */
 ]);
 //var interval = setInterval(getResults, 100);
 //var intervalT = setInterval(getResultsT, 100);
